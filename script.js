c78f621bebffba99db3c5be269ad2ec5a713e509
Vicky Ricart <vickyc.ricart@gmail.com>
2024-05-22 15:09:49 +0200

    Update

--- script.js
+++ script.js
@@ -1,63 +1,69 @@
 const tileImageArr = [
-    'tile-1.png',
-    'tile-2.png',
-    'tile-3.png',
-    'tile-4.png',
-    'tile-5.png',
-    'tile-6.png',
-    'tile-7.png',
-    'tile-8.png',
-    'tile-9.png',
-    'tile-10.png',
-    'tile-11.png',
-    'tile-12.png'
+  'tile-1.png',
+  'tile-2.png',
+  'tile-3.png',
+  'tile-4.png',
+  'tile-5.png',
+  'tile-6.png',
+  'tile-7.png',
+  'tile-8.png',
+  'tile-9.png',
+  'tile-10.png',
+  'tile-11.png',
+  'tile-12.png'
 ];
 
 
 function getRandomTile(element) {
-    const randomIndex = Math.floor(Math.random() * tileImageArr.length);
-    const selectedTile = tileImageArr[randomIndex];
-    console.log(selectedTile);
-    element.src = `./assets/${selectedTile}`;
+  const randomIndex = Math.floor(Math.random() * tileImageArr.length);
+  const selectedTile = tileImageArr[randomIndex];
+  console.log(selectedTile);
+  element.src = `./assets/${selectedTile}`;
 }
 
 
 function toggleNav() {
-    const sidenav = document.getElementById("mySidenav");
-    const menuCheckbox = document.getElementById("menu-checkbox");
-    if (menuCheckbox.checked) {
-      sidenav.style.width = "100%";
-    } else {
-      sidenav.style.width = "0";
-    }
+  const sidenav = document.getElementById("mySidenav");
+  const menuCheckbox = document.getElementById("menu-checkbox");
+  if (menuCheckbox.checked) {
+    sidenav.style.width = "100%";
+  } else {
+    sidenav.style.width = "0";
   }
+}
 
 
-  document.getElementById("menu-checkbox").addEventListener("change", toggleNav);
+document.getElementById("menu-checkbox").addEventListener("change", toggleNav);
 
 
-  document.querySelectorAll('.hover-target').forEach(element => {
-    element.addEventListener('click', () => {
-        const popup = element.nextElementSibling;
-        popup.style.display = popup.style.display === 'block' ? 'none' : 'block';
-    });
+document.querySelectorAll('.sidenav-link').forEach(link => {
+  link.addEventListener('click', () => {
+      const sidenav = document.getElementById("mySidenav");
+      const menuCheckbox = document.getElementById("menu-checkbox");
+      sidenav.style.width = "0";
+      menuCheckbox.checked = false;
+  });
 });
 
-
-document.querySelectorAll('.project-img').forEach(image => {
-  image.addEventListener('click', () => {
-      const overlay = image.nextElementSibling;
-      overlay.style.opacity = overlay.style.opacity === '1' ? '0' : '1';
+document.querySelectorAll('.hover-target').forEach(element => {
+  element.addEventListener('click', () => {
+      const popup = element.nextElementSibling;
+      popup.style.display = popup.style.display === 'block' ? 'none' : 'block';
   });
 });
 
 
 document.addEventListener("DOMContentLoaded", function() {
-  const projectContainers = document.querySelectorAll('.project-container');
+const projectContainers = document.querySelectorAll('.project-container');
 
-  projectContainers.forEach(container => {
-      container.addEventListener('click', () => {
-          container.classList.toggle('show-overlay');
-      });
-  });
+projectContainers.forEach(container => {
+    container.addEventListener('click', () => {
+        container.classList.toggle('show-overlay');
+        projectContainers.forEach(otherContainer => {
+            if (otherContainer !== container) {
+                otherContainer.classList.remove('show-overlay');
+            }
+        });
+    });
+});
 });
